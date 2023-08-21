15b14e155f9fd7a24b345dc7560a1c63eeb69449
mjschafer <140786254+mjschafer@users.noreply.github.com>
2023-08-20 21:44:33 -0500

    Add index.html file

--- azure_personal_website_app/index.js
+++ azure_personal_website_app/index.js
@@ -1,9 +1,28 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const url = require('url');
+const fs = require('fs');
+
 
 http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World! This is Mackenzie :)');
+    onRequest(request, response);
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
+
+function createFileResponse(err, data, response) {
+    if (err) {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        return response.end("404 Not Found");
+    } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        return response.end();
+    }
+}
+
+function onRequest(request, response) {
+    fs.readFile('./index.html', function(err, data) {
+        createFileResponse(err, data, response);
+    });
+}
